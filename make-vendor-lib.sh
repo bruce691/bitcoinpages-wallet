b8ecbfe3f47a0980ec47665c98e57b01166f9d1a
jaxonlee179 <nataliehall179@gmail.com>
2018-04-17 21:12:21 +0200

    cleanup

--- make-vendor-lib.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-./node_modules/browserify/bin/cmd.js  -r bip39 -s bip39  > ./src/vendor/bip39.js
-./node_modules/browserify/bin/cmd.js -r bitcoinjs-lib -s bitcoin  > ./src/vendor/bitcoinjs-lib.js
